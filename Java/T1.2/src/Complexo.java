7f2ca018fde3afa94b2e0ef0b12e0121423948a5
mosconibruno <contato.brunomosconi@gmail.com>
2018-03-01 14:24:53 -0300

    Bruno: Criei o projeto T1.2

---
+++ Java/T1.2/src/Complexo.java
@@ -0,0 +1,31 @@
+
+public class Complexo {
+	
+	double a, b;
+	
+	void Construtor() {
+		
+	}
+	
+	void soma() {
+		
+	}
+	
+	void multiplica() {
+		
+	}
+	
+	void paraString() {
+		
+	}
+	
+	void modulo() {
+		
+	}
+	
+	void argumentoPrincipal() {
+		
+	}
+	
+	
+}
