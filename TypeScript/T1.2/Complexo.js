7d6b8e1c48d5b126325b1d89566c5dab6674647d
mosconibruno <contato.brunomosconi@gmail.com>
2018-03-02 16:02:05 -0300

    Bruno: Criei projeto T1.2

---
+++ TypeScript/T1.2/Complexo.js
@@ -0,0 +1,22 @@
+var Greeter = (function () {
+    function Greeter() {
+    }
+    return Greeter;
+}());
+var Complexo = (function () {
+    function Complexo() {
+    }
+    Complexo.prototype.Construtor = function () {
+    };
+    Complexo.prototype.soma = function () {
+    };
+    Complexo.prototype.multiplica = function () {
+    };
+    Complexo.prototype.paraString = function () {
+    };
+    Complexo.prototype.modulo = function () {
+    };
+    Complexo.prototype.argumentoPrincipal = function () {
+    };
+    return Complexo;
+}());
---
+++ TypeScript/T1.2/Complexo.ts
@@ -0,0 +1,34 @@
+class Greeter {
+     
+}
+
+class Complexo{
+    public a;
+    public b;
+    
+    public Construtor() {
+        
+    }
+    
+    public soma() {
+        
+    }
+    
+    public multiplica() {
+        
+    }
+    
+    public paraString() {
+        
+    }
+    
+    public modulo() {
+        
+    }
+    
+    public argumentoPrincipal() {
+        
+    }
+    
+
+}
