72130702036c0e9440c00f1d5a0c1a1b0ad7c5b8
mosconibruno <contato.brunomosconi@gmail.com>
2018-03-04 02:49:16 -0300

    T1.2 Java

--- Java/T1.2/src/Complexo.java
+++ Java/T1.2/src/Complexo.java
@@ -3,29 +3,48 @@ public class Complexo {
 	
 	double a, b;
 	
-	void Construtor() {
+	void Construtor(double a, double b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	void soma(double c, double d) {
+		
+		System.out.println("A soma de " + c + "+" + d + "i com " + this.a + "+" + this.b + "i é: " + (this.a + c) + "+ "+ (this.b + d) + "i");
 		
 	}
 	
-	void soma() {
+	void multiplica(double c, double d) {
+		
+		System.out.println("A multiplicacao de " + c + "+" + d + "i com " + this.a + "+" + this.b + "i é: " + ((this.a * c) - (this.b * d)) + "+" + ((this.a * d) + (this.b * c)) + "i");
 		
 	}
 	
-	void multiplica() {
+	void mostrar() {
+		
+		System.out.println(this.a + "+" + this.b + "i");
 		
 	}
 	
-	void paraString() {
+	double modulo() {
+			
+		return Math.sqrt(Math.pow(this.a, 2) + Math.pow(this.b, 2));
+				
+	}
+	
+	double argumentoPrincipal() {
+		
+	/*	double tangente, cosseno, seno;
+		cosseno = this.a / this.modulo();
+		seno = this.b / this.modulo();
+		tangente = seno / cosseno; */
+		
+		return Math.pow(((this.b/this.modulo())/(this.a/this.modulo())), -1) * (this.a / this.b);
 		
 	}
 	
-	void modulo() {
-		
-	}
-	
-	void argumentoPrincipal() {
-		
-	}
-	
+
 	
 }
+
+
